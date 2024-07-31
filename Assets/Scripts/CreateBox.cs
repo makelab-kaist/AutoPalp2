4516894a165ab53cb576b807b35d0c7b28f7cb9f
yumin-jung <jungym887@naver.com>
2024-07-31 17:30:38 +0900

    refactor: Refactor CreateBox.cs

--- Assets/Scripts/CreateBox.cs
+++ Assets/Scripts/CreateBox.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class CreateBox : MonoBehaviour
 {
-    public GameObject Panel;
     public GameObject Cube;
 
     public void OpenPanel()
     {
-        if (Panel != null && Cube != null)
+        if (Cube != null)
         {
-            bool isPanelActive = Panel.activeSelf;
             bool isCubeActive = Cube.activeSelf;
-            Panel.SetActive(!isPanelActive);
             Cube.SetActive(!isCubeActive);
         }
     }
