f58357605e8140c48418bd3dc276fb1fada7ae45
yumin-jung <jungym887@naver.com>
2024-08-01 14:41:49 +0900

    feat: Add arduino connection

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Add this to use TextMeshPro
 
 using NativeWebSocket;
 
 public class Connection : MonoBehaviour
 {
     WebSocket websocket;
+    public TextMeshPro textMeshPro; // Reference to the TextMeshPro component
 
     // Start is called before the first frame update
     async void Start()
@@ -32,11 +34,13 @@ public class Connection : MonoBehaviour
         websocket.OnMessage += (bytes) =>
         {
             Debug.Log("OnMessage!");
-            Debug.Log(bytes);
 
-            // getting the message as a string
-            // var message = System.Text.Encoding.UTF8.GetString(bytes);
-            // Debug.Log("OnMessage! " + message);
+            // Decode the byte array to a string
+            var message = System.Text.Encoding.UTF8.GetString(bytes);
+            Debug.Log("Received from Arduino: " + message);
+
+            // Update the 3D text
+            UpdateText(message);
         };
 
         // Keep sending messages at every 0.3s
@@ -62,6 +66,9 @@ public class Connection : MonoBehaviour
 
             // Sending plain text
             await websocket.SendText("Hello from Meta Quest 3");
+
+            // For Arduino
+            await websocket.SendText("A");
         }
     }
 
@@ -70,4 +77,11 @@ public class Connection : MonoBehaviour
         await websocket.Close();
     }
 
+    void UpdateText(string message)
+    {
+        if (textMeshPro != null)
+        {
+            textMeshPro.text = "Arduino: " + message;
+        }
+    }
 }
