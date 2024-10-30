22896d3996ff2c722ebbb487765c1dfe75af7867
yumin-jung <jungym887@naver.com>
2024-10-30 17:22:40 +0900

    fix: Fix pain scale direction error

--- Assets/Scripts/RestrictXAxis.cs
+++ Assets/Scripts/RestrictXAxis.cs
@@ -5,7 +5,7 @@ public class RestrictXAxis : MonoBehaviour
     void Update()
     {
         Vector3 currentPosition = transform.position;
-        currentPosition.x = Mathf.Clamp(currentPosition.x, -0.12f, 0.08f);
+        currentPosition.x = Mathf.Clamp(currentPosition.x, 0.294f, 0.446f);
         transform.position = currentPosition;
     }
 }
